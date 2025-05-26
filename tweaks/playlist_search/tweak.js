8e9a9e746bda03f12d88f2b2cc51e3a2e8bca578
emkab <me@emkab.dev>
2025-05-26 15:26:52 +0300

    Add CSS.

--- tweaks/playlist_search/tweak.js
+++ tweaks/playlist_search/tweak.js
@@ -1,3 +1,8 @@
+const link = document.createElement("link");
+link.rel = "stylesheet";
+link.href = "tweak.css";
+document.head.appendChild(link);
+
 const s_video_list = "#contents.ytd-playlist-video-list-renderer";
 const s_video = "#contents > ytd-playlist-video-renderer:nth-child";
 const s_title_postfix = " #video-title";
@@ -12,13 +17,55 @@ const e_playlist_search_input = document.createElement("input", {
 	type: "search",
 	name: "ek_yt_tweaks_playlist_search_input",
 	id: "ek_yt_tweaks_playlist_search_input",
+	autocomplete: "on",
 });
-const e_playlist_search_form = document.createElement("form");
+const e_playlist_search_form = document.createElement("form", {
+	id: "ek_yt_tweaks_playlist_search_form",
+});
 const e_playlist_search_submit_button = document.createElement("button");
 e_playlist_search_submit_button.textContent = "Search";
 e_playlist_search_form.appendChild(e_playlist_search_input_label);
 e_playlist_search_form.appendChild(e_playlist_search_input);
 e_playlist_search_form.appendChild(e_playlist_search_submit_button);
+
+e_playlist_search_form.style.color =
+	"var(--ytd-searchbox-legacy-button-icon-color)";
+e_playlist_search_form.style.backgroundColor = "var(--yt-spec-base-background)";
+e_playlist_search_form.style.width = "90%";
+e_playlist_search_form.style.height = "32px";
+e_playlist_search_form.style.marginBottom = "15px";
+e_playlist_search_form.style.display = "flex";
+e_playlist_search_form.style.justifyContent = "between";
+e_playlist_search_form.style.alignItems = "stretch";
+e_playlist_search_form.style.padding = "0 36px";
+
+e_playlist_search_input_label.style.display = "none";
+e_playlist_search_input.style.display = "inline-block";
+e_playlist_search_submit_button.style.display = "inline-block";
+
+e_playlist_search_input.style.width = "80%";
+e_playlist_search_submit_button.style.width = "10%";
+
+e_playlist_search_input.style.color =
+	"var(--ytd-searchbox-legacy-button-icon-color)";
+e_playlist_search_input.style.backgroundColor =
+	"var(--yt-spec-menu-background)";
+e_playlist_search_input.style.borderRadius = "12px";
+e_playlist_search_input.style.border = "none";
+e_playlist_search_input.style.fontSize = "1.45em";
+e_playlist_search_input.style.padding = "0 10px";
+e_playlist_search_input.style.outline = "none";
+
+e_playlist_search_submit_button.style.color =
+	"var(--ytd-searchbox-legacy-button-icon-color)";
+e_playlist_search_submit_button.style.backgroundColor =
+	"var(--yt-spec-menu-background)";
+e_playlist_search_submit_button.style.borderRadius = "12px";
+e_playlist_search_submit_button.style.border = "none";
+e_playlist_search_submit_button.style.cursor = "pointer";
+e_playlist_search_submit_button.style.marginLeft = "5px";
+e_playlist_search_submit_button.style.fontSize = "1.45em";
+e_playlist_search_submit_button.style.fontWeight = "semibold";
 
 async function init() {
 	await sleep(500);
